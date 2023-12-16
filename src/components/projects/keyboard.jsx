d8f5c7423cca429a118cf092409463bbabc24510
Brian Perrett <perrettbrian@gmail.com>
2023-12-15 19:43:57 -0800

    update keyboard embed

--- src/components/projects/keyboard.jsx
+++ src/components/projects/keyboard.jsx
@@ -17,7 +17,7 @@ export default function Keyboard(props) {
 
                     <div style={{paddingTop: "60%", position: 'relative'}}>
                         <iframe
-                            src="https://configure.zsa.io/embed/voyager/layouts/KY3L9/wRlyj/0"
+                            src="https://configure.zsa.io/embed/voyager/layouts/KY3L9/A5e6B/0"
                             style={{border: 0, height: '100%', left: 0, position: 'absolute', top: 0, width: '100%'}}
                         >
                         </iframe>
